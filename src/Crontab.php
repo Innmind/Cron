e26ab60683822322fdc5865d47884f691a6323ae
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-07 15:34:52 +0200

    add crontab installer

---
+++ src/Crontab.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Cron;
+
+use Innmind\Server\Control\{
+    Server,
+    Server\Command,
+    Server\Script,
+};
+use Innmind\Immutable\Sequence;
+
+final class Crontab
+{
+    private $command;
+    private $jobs;
+
+    private function __construct(Command $command, Job ...$jobs)
+    {
+        $this->command = Command::foreground('echo')
+            ->withArgument((string) Sequence::of(...$jobs)->join("\n"))
+            ->pipe($command);
+    }
+
+    public static function forConnectedUser(Job ...$jobs): self
+    {
+        return new self(Command::foreground('crontab'), ...$jobs);
+    }
+
+    public static function forUser(string $user, Job ...$jobs): self
+    {
+        return new self(
+            Command::foreground('crontab')
+                ->withShortOption('u', $user),
+            ...$jobs
+        );
+    }
+
+    public function __invoke(Server $server): void
+    {
+        $installOn = new Script($this->command);
+
+        $installOn($server);
+    }
+}
